496e66a0e9438ee88bfeeafebbe3ed6bf4dc584d
Harrison Bailey <harry@Harrisons-MacBook-Pro.local>
2024-10-11 16:46:14 +1100

    Added better text upload and button functionality

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from langchain import Document
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain.tools.retriever import create_retriever_tool
+from langchain import hub
+from langchain_ollama import ChatOllama
+
+# The UI Part
+st.title("👨‍💻 Let's chat about the meeting!")
+uploaded_file = st.file_uploader("Upload a .txt file of the meeting transcript here", type=["txt"])
+load_button = st.button("Load meeting transcript.", type="primary")
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+
+# Load VectorDB
+if load_button: 
+    if uploaded_file:
+        text = uploaded_file.getvalue()
+        st.write(text)
+        st.write(uploaded_file.read().decode())
+        docs = Document(page_content=text.decode('utf-8'))
+        st.write(text)
+        # Change UI
+        st.text("You did it!")
+        transcript = None
+        load_button = None
+        prompt = st.text_area("Please enter what you want to know from the meeting.")
+    else:
+        st.write("MUST UPLOAD FILE!")
+
+if uploaded_file and question:
+    st.write("Nice")
