5c051aa9876362f5d12270bddeb465d2da83bbf5
Harrison Bailey <harry@Harrisons-MacBook-Pro.local>
2024-10-11 18:28:01 +1100

    Added UI changes

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 # The UI Part
 st.title("👨‍💻 Let's chat about the meeting!")
 uploaded_file = st.file_uploader("Upload a .txt file of the meeting transcript here", type=["txt"])
-load_button = st.button("Load meeting transcript.", type="primary")
+load_button = st.button("Load meeting transcript", type="primary")
 # Placeholder for later
-rag = None
+rag = st.empty()
+warning_text = st.empty()
 question = st.text_area(
     "Please enter what you want to know from the meeting.",
     placeholder="Can you give me a short summary?",
@@ -18,17 +19,25 @@ question = st.text_area(
 # Load VectorDB
 if load_button: 
     if uploaded_file:
+        # Update IU
+        warning_text.empty()
         # text = uploaded_file.getvalue()
         text = uploaded_file.read().decode()
         # Set up RAG instance
         rag = RAG(text)
     else:
-        st.write("MUST UPLOAD FILE!")
+        # Update UI
+        warning_text.text("MUST UPLOAD FILE!")
 
 # Process question
 if uploaded_file and question:
-    # Run process query on RAG instance
-    stream = rag.process_query(question)
-    # Write out stream on site
-    print(stream)
-    st.write_stream(stream)
+    if rag:
+        # Update UI
+        warning_text.empty()
+        # Run process query on RAG instance
+        stream = rag.process_query(question)
+        # Write out stream on site
+        st.write_stream(stream)
+    else:
+        # Update UI
+        warning_text.text("Please load the transcript first")
