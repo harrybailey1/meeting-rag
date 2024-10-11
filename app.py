ac9c151f570bff3940af129a15af3735fb25b8a8
Harrison Bailey <harry@Harrisons-MacBook-Pro.local>
2024-10-11 17:17:05 +1100

    Changed print

--- app.py
+++ app.py
@@ -10,19 +10,20 @@ from langchain_ollama import ChatOllama
 st.title("👨‍💻 Let's chat about the meeting!")
 uploaded_file = st.file_uploader("Upload a .txt file of the meeting transcript here", type=["txt"])
 load_button = st.button("Load meeting transcript.", type="primary")
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
+# question = st.text_area(
+#     "Now ask a question about the document!",
+#     placeholder="Can you give me a short summary?",
+#     disabled=not uploaded_file,
+# )
+prompt = None
 
 
 # Load VectorDB
 if load_button: 
     if uploaded_file:
         text = uploaded_file.getvalue()
-        print(text, type(text))
-        print(uploaded_file.read().decode(), type(uploaded_file.read().decode()))
+        print(type(text))
+        print(type(uploaded_file.read().decode()))
         # docs = Document(page_content=text.decode('utf-8'))
         # Change UI
         st.text("You did it!")
@@ -32,5 +33,5 @@ if load_button:
     else:
         st.write("MUST UPLOAD FILE!")
 
-if uploaded_file and question:
+if uploaded_file and prompt:
     st.write("Nice")
